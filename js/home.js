4e5ab7638d75f85614ab51d9116b7577326d8126
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-06-12 14:11:27 +0600

    added new home.js file

---
+++ js/home.js
@@ -0,0 +1,15 @@
+//console.log("connected");
+var canvas = document.getElementById("mycanvas");
+var c = canvas.getContext("2d");
+
+//body
+c.beginPath();
+
+c.fillStyle = "Black";
+
+c.rect(80,50,80,60);
+c.fill();
+
+
+
+c.closepath();
