68d3de441aa5dc91d60a22284908b1175f205935
Mahtab-Hossain <alokmahtab88@gmail.com>
2022-06-12 14:28:11 +0600

    modified

--- js/home.js
+++ js/home.js
@@ -1,6 +1,7 @@
 //console.log("connected");
 var canvas = document.getElementById("mycanvas");
 var c = canvas.getContext("2d");
+var d = canvas.getContext("3d");
 
 //body
 c.beginPath();
@@ -10,6 +11,8 @@ c.fillStyle = "Black";
 c.rect(80,50,80,60);
 c.fill();
 
+c.fillStyle = "Red";
+c.rect(170,50,80,60);
+c.fill();
 
-
-c.closepath();
+c.closePath();
